cf80e13b160cf9a0f7575ee515cb0647cbbd8214
Surly Meyer <89383@sistemas.frc.utn.edu.ar>
2025-08-25 09:27:15 -0300

    semana 3 de clases

---
+++ Teorico/ejercicio-tamagotchi/src/main/java/ar/edu/utnfc/backend/App.java
@@ -0,0 +1,9 @@
+package ar.edu.utnfc.backend;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Teorico/ejercicio-tamagotchi/src/main/java/ar/edu/utnfc/backend/modelo/Mascota.java
@@ -0,0 +1,17 @@
+package ar.edu.utnfc.backend.modelo;
+
+public class Mascota {
+    // Atributos
+    private String nombre;
+    private int energia;
+    private int humor;
+
+    // Constructor
+    public Mascota(String nom, int energia, int humor) {
+        nombre = nom;
+        energia = energia;
+        humor = humor;
+    }
+
+    public comer
+}
